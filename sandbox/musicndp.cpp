0a217d8c191c403ad50553d47f864312227301ae
phuc <dinhphucaz52@gmail.com>
2025-11-09 19:45:35 +0700

    feat: add initial musicndp files and update configuration

---
+++ sandbox/musicndp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_1.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_2.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_3.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_4.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_5.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_6.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_7.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_8.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_9.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_10.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_11.json\n");
+    printf("/home/phuc/RustRoverProjects/yazi/sandbox/test_12.json\n");
+    return 0;
+}
